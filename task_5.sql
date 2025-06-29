310a6bec7def5d4a344911027131a85f1fdd87d4
Oyembo Peninah <aumaoyembo@gmail.com>
2025-06-29 14:29:58 +0300

    Add recent file edits

---
+++ task_5.sql
@@ -0,0 +1,18 @@
+import mysql.connector
+mydb = mysql.connector.connect(
+    host = "localhost",
+    user = "root"
+    password = "yourpassword"
+    database = "alx_book_store"
+)
+mycursor = alx_book_store.cursor()
+
+USE alx_book_store;
+WHERE
+    TABLE_SCHEMA = 'alx_book_store'
+    TABLE_NAME = 'Customers'
+
+sql = INSERT INTO Customers (customer_id, customer_name, email, address) VALUES (%s, %s, %s, %s)
+val = ("1", "Cole Baidoo", "cbaidoo@sandtech.com", "123 Happiness Ave.")
+mycursor.execute(sql, val)
+alx_book_store.commit()
