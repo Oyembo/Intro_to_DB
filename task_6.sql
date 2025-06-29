bdbb5d74bc27d93d89f9e166800ad25cb69c48f1
Oyembo Peninah <aumaoyembo@gmail.com>
2025-06-29 14:48:54 +0300

    Add edits

--- task_6.sql
+++ task_6.sql
@@ -13,8 +13,10 @@ WHERE
     TABLE_NAME = 'customer'
 
 sql = INSERT INTO customer (customer_id, customer_name, email, address) VALUES (%s, %s, %s, %s)
-val = ("2", "Blessing Malik", "bmalik@sandtech.com", "124 Happiness Ave.""),
-      ("3", "Obed Ehoneah", "eobed@sandtech.com", "125 Happiness Ave." "),
-      ("4", "Nehemial Kamolu", "nkamolu@sandtech.com", "126 Happiness Ave."")
+val = [
+      ("2", "Blessing Malik", "bmalik@sandtech.com", "124 Happiness Ave."),
+      ("3", "Obed Ehoneah", "eobed@sandtech.com", "125 Happiness Ave."),
+      ("4", "Nehemial Kamolu", "nkamolu@sandtech.com", "126 Happiness Ave.")
+]
 mycursor.execute(sql, val)
 alx_book_store.commit()
