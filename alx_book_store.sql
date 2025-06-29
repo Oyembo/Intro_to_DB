dd435c54734d4aa081e4137e97d90de0e2173905
Oyembo Peninah <aumaoyembo@gmail.com>
2025-06-29 13:15:02 +0300

    Add recent edits

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -5,11 +5,12 @@ mydb = mysql.connector.connect(
     password = "hanineP3049#"
     database = "alx_book_store"
 )
+CREATE DATABASE IF NOT EXISTS alx_book_store
 
 mycursor = alx_book_store.cursor()
 mycursor.execute("""
 
-CREATE TABLE IF NOT EXIST Books (
+CREATE TABLE IF NOT EXISTS Books (
     book_id PRIMARY KEY,
     title VARCHAR(130) NOT NULL,
     author_id (FOREIGN KEY REFERENCING AUTHORS TABLE)
@@ -18,22 +19,22 @@ CREATE TABLE IF NOT EXIST Books (
 )
 print("Table created successfully!")
 
-CREATE TABLE IF NOT EXIST Authors(
+CREATE TABLE IF NOT EXISTS Authors(
     author_id PRIMARY KEY,
     author_name VARCHAR(215) NOT NULL
 ),
-CREATE TABLE IF NOT EXIST Customers(
+CREATE TABLE IF NOT EXISTS Customers(
     customer_id PRIMARY KEY,
     customer_name VARCHAR(215) NOT NULL,
     email VARCHAR(215) NOT NULL,
     address TEXT
 ),
-CREATE TABLE IF NOT EXIST Orders(
+CREATE TABLE IF NOT EXISTS Orders(
     order_id PRIMARY KEY,
     customer_id FOREIGN KEY
     order_date DATE
 ),
-CREATE TABLE IF NOT EXIST Order_Details(
+CREATE TABLE IF NOT EXISTS Order_Details(
     orderdetailid PRIMARY KEY
     order_id FOREIGN KEY
     book_id FOREIGN KEY
