80530d0ae8e20e54e793379e278ac8e8d3a3a7c2
Oyembo Peninah <aumaoyembo@gmail.com>
2025-06-29 14:14:52 +0300

    Add file edits

--- task_4.sql
+++ task_4.sql
@@ -7,8 +7,20 @@ mydb = mysql.connector.connect(
 )
 SHOW DATABASE;
 USE alx_book_store;
-SELECT TABLE;
-WHERE;
-    TABLE_SCHEMA = Database()
+SELECT
+    COLUMN_NAME AS `Field`,
+    COLUMN_TYPE AS `Type`,
+    CASE
+        WHEN IS_NULLABLE = 'YES' THEN 'YES'
+        ELSE 'NO'
+    END AS `Null`,
+    COLUMN_KEY AS `Key`,
+    COLUMN_DEFAULT AS `Default`,
+    EXTRA AS `Extra`
+FROM
+    information_schema.COLUMNS
+WHERE
+    TABLE_SCHEMA = DATABASE()
     TABLE_NAME = 'Books'
-print("Contents updated")
+ORDER BY
+    ORDINAL_POSITION;
