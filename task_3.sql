de26f694f168939c5f2c47ae41411d16cdc4ec6c
Oyembo Peninah <aumaoyembo@gmail.com>
2025-06-29 13:59:34 +0300

    Add file edits

---
+++ task_3.sql
@@ -0,0 +1,11 @@
+import mysql.connector
+mydb = mysql.connector.connect(
+    host = "localhost",
+    user = "root"
+    password = "yourpassword"
+    database = "alx_book_store"
+)
+SHOW DATABASE,
+USE alx_book_store,
+SHOW TABLES,
+print("Tables present")
