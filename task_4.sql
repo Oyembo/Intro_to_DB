b97e22e87b7913b5125ed46dc580c97d1c722059
Oyembo Peninah <aumaoyembo@gmail.com>
2025-06-29 14:12:32 +0300

    Add file edits

---
+++ task_4.sql
@@ -0,0 +1,14 @@
+import mysql.connector
+mydb = mysql.connector.connect(
+    host = "localhost",
+    user = "root"
+    password = "yourpassword"
+    database = "alx_book_store"
+)
+SHOW DATABASE;
+USE alx_book_store;
+SELECT TABLE;
+WHERE;
+    TABLE_SCHEMA = Database()
+    TABLE_NAME = 'Books'
+print("Contents updated")
