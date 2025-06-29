8c704c1845096b8dc521c0920914fca37ec9e3a3
Oyembo Peninah <aumaoyembo@gmail.com>
2025-06-29 13:24:47 +0300

    Add file edits

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -31,13 +31,13 @@ CREATE TABLE IF NOT EXISTS Customers(
 ),
 CREATE TABLE IF NOT EXISTS Orders(
     order_id INT PRIMARY KEY,
-    customer_id FOREIGN KEY (customer_id) REFERENCES Customers
+    customer_id FOREIGN KEY (customer_id) REFERENCES Customers(customer_id),
     order_date DATE
 ),
 CREATE TABLE IF NOT EXISTS Order_Details(
     orderdetailid PRIMARY KEY
     order_id FOREIGN KEY (order_id) REFERENCES Orders(order_id),
-    book_id INT FOREIGN KEY (book_id) REFERENCES Books(book_id)
+    book_id INT FOREIGN KEY (book_id) REFERENCES Books(book_id),
     quantity DOUBLE
 )
 """)
