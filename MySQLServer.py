64f7b1f783c09f46c680081d57fe3afdc6389d7c
Oyembo Peninah <aumaoyembo@gmail.com>
2025-06-29 13:46:58 +0300

    Add recent file edits

---
+++ MySQLServer.py
@@ -0,0 +1,18 @@
+import mysql.connector
+alx_book_store = mysql.connector.connect(
+    host = "localhost"
+    user = "root"
+    password = "yourpassword"
+    database = alx_book_store
+)
+
+mycursor = alx_book_store.cursor()
+
+mycursor.execute("""
+CREATE DATABASE IF NOT EXISTS alx_book_store(
+)
+""")
+print("Database 'alx_book_store' created successfully!")
+
+else:
+    print("Error occured, try again!")
